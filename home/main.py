e0125abcc9b33b041b7112260d09d389ca9a8ca8
smhty <hussein.ty@gmail.com>
2023-06-23 16:24:51 -0700

    Update main.py

--- home/main.py
+++ home/main.py
@@ -20,42 +20,41 @@ homing process for joint i starts
 2,000,0i7: error, clear alarm
 2,000,0i8: error, reset pid 
 """
-def home(robot, index, val, direction, **kwargs):
+def home(robot, index, val, direction, thr, dur, **kwargs):
     # activate the motors
-    result = robot.set_motor(1)
-    if result != 2:
+    if robot.set_motor(1) != 2:
         # error happened
-        return home_error_handling(robot, index)     
-    # set thereshould
+        return home_error_handling(robot, index, thr, dur)     
+    
+    # set threshold
     id = 2000000+10*index # 0
-    result = robot.set_pid(threshold=10, duration=5, id=id)
-    if result != 2:
+    if robot.set_err_thr(5) != 2 or robot.set_err_dur(5, id=id) != 2:
         # error happened
-        return home_error_handling(robot, index) 
-    # set joint
+        return home_error_handling(robot, index, thr, dur) 
+    
+    print("after: ",robot.get_err_thr(), robot.get_err_dur())
+    # motion
     id += 1 # 1
     joint = "j"+str(index)
     # move in the given direction with the given speed
     arg = {"vel": kwargs["vel_forward"], "rel":1, joint: kwargs["forward"] * direction, "id": id}  
-    result = robot.jmove(**arg) # move toward the homing direction until the alarm
-    if result >= 0:
+    if robot.jmove(**arg) >= 0:
         # error happened
-        return home_error_handling(robot, index) 
+        return home_error_handling(robot, index, thr, dur) 
 
-    # sleep
+    # sleep at alarm
     time.sleep(0.2)
 
     # clear the alarm
     id += 1 # 2
-    result = robot.set_alarm(0, id=id)
-    if result != 2:
+    if robot.set_alarm(0, id=id) != 2:
         # error happened
-        return home_error_handling(robot, index)
+        return home_error_handling(robot, index, thr, dur)
 
     # reset pid
     id += 1 # 3
-    result = robot.reset_pid(id=id)
-    if result != 2:
+    #result = robot.reset_pid(id=id)
+    if robot.set_err_thr(thr) !=2 or robot.set_err_dur(dur, id=id) !=2:
         # error happened
         return home_error_handling(robot, index) 
     
@@ -67,34 +66,33 @@ def home(robot, index, val, direction, **kwargs):
         # set the probe
         id += 1 # 4
         iprobe = robot.iprobe(index, kwargs["iprobe_val"], id=id) # wait for the input trigger
-
+        
         # halt
         id += 1 # 5
-        result = robot.halt(kwargs["halt_accel"], id=id)
-        if result != 2:
+        if robot.halt(kwargs["halt_accel"], id=id) != 2:
             # error happened
-            return home_error_handling(robot, index)
-
+            return home_error_handling(robot, index, thr, dur)
+        
     time.sleep(1)
 
     # set joint
     joint_assignment = val + robot.val(joint) - iprobe[index]
     id += 1 # 6
-    result = robot.set_joint(index, joint_assignment, id=id)
-    if result != 2:
+    if robot.set_joint(index, joint_assignment, id=id) != 2:
         # error happened
-        return home_error_handling(robot, index)
+        return home_error_handling(robot, index, thr, dur)
 
-    #arg = {"rel": 0, joint: kwargs["stop"]}
-    #robot.jmove(**arg)
     return True
 
 
-def home_error_handling(robot, index):
+def home_error_handling(robot, index, thr, dur):
     id = 2000000+10*index+7 # 7
     robot.set_alarm(0, id=id)
     id += 1 # 8
-    robot.reset_pid(id=id)
+    #robot.reset_pid(id=id)
+    robot.set_err_thr(thr)
+    robot.set_err_dur(dur, id=id)
+
 if __name__ == '__main__':
     # Initialize parser
     parser = argparse.ArgumentParser()
@@ -116,9 +114,15 @@ if __name__ == '__main__':
     robot = Dorna()
     robot.connect(host)
 
+    # original thr, dur
+    thr = robot.get_err_thr()
+    dur = robot.get_err_dur()
+
     robot.log("connected")
     for i in range(1):
-        home(robot, index, val, direction, **config["j"+ str(index)])
+        home(robot, index, val, direction, thr, dur, **config["j"+ str(index)])
         time.sleep(1)
+    
+    print("before: ", robot.get_err_thr(), robot.get_err_dur())
     robot.close()
     
