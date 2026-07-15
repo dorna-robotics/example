dac67264102c3300d553d2dee65a7dfb70398cbb
smhty <hussein.ty@gmail.com>
2026-07-15 10:45:45 -0700

    output setting

--- io/main.py
+++ io/main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 
     # connect
     if robot.connect():
-        output_setting = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        output_setting = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         for i in range(min(len(output_setting), 16)):
             robot.set_output(i, output_setting[i])
     robot.close()
